3ba7ba2691fc6ca3f3a162fc0d46d9c5edd6967e
Srivatsan <vatsan1993@gmail.com>
2024-08-15 17:46:46 +0530

    code with error

--- fetch-requests.js
+++ fetch-requests.js
@@ -10,9 +10,13 @@ Paste your code for fetch requests here once you finish each task.
 */
 
 // Your code here
+async function getPosts() {
+  let url = 'http://localhost:5000/posts';
+  let response = await fetch(url);
+  let data = await response.json();
+}
 
-
-
+getPosts();
 /* =============================== Phase 2 ================================ */
 /*
   Make a request with fetch request to POST /posts and print the response
