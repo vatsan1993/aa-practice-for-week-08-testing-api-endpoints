2c7b81e2f0f7e6726ac013eb1f33e1be431bcb51
Srivatsan <vatsan1993@gmail.com>
2024-08-15 17:48:16 +0530

    added a log statement

--- fetch-requests.js
+++ fetch-requests.js
@@ -14,6 +14,7 @@ async function getPosts() {
   let url = 'http://localhost:5000/posts';
   let response = await fetch(url);
   let data = await response.json();
+  console.log(data);
 }
 
 getPosts();
